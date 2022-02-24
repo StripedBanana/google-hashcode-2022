b3b4b7b1fd72c4ea215257780c19e4b20f8e594a
NAMDAR Julien <julien.namdar@soprasteria.com>
2022-02-24 21:03:06 +0100

    Ajout check projects

--- practice/model/contributor.py
+++ practice/model/contributor.py
@@ -2,3 +2,4 @@ class Contributor:
     def __init__(self, name, skills):
         self.name = name
         self.skills = skills
+        self.busy = False
--- practice/run.py
+++ practice/run.py
@@ -6,32 +6,43 @@ from model.contributor import Contributor
 
 import model.project
 
+Projects = list[Project]
 def run(contrib, projects):
 
     # day 0
     D = 0
+
+    # finished projects
+    finished_projects: Projects = []
     while(True):
 
         update_skill_levels(projects)
 
         add_project(projects)
 
-        update_contrib(contrib)
-
-
+        finished_projects = check_projects(projects, finished_projects, D)
 
         D+=1
 
-Projects = list[Project]
+
 def update_skill_levels(projects: Projects):
     for project in projects:
         for k, v in projects.roles.items():
             if v.contributor.skills[k] >= v.cap:
                 v.contributor.skills[k] += 1
         
+def check_projects(projects: Projects, finished_projects: Projects, D):
+    for project in projects:
+        for k, v in project.roles.items():
+            if v.contributor != "":
+                # if every role is filled, decrement project required time
+                project.requiredTime -= 1
+        if project.requiredTime == 0 or project.bestBeforeCompletion < D:
+            # if project is done
+            finished_projects.append(project)
+            for contributor in project.contributors:
+                contributor.busy = False
+
 
 def add_project():
-    print("t")
-
-def update_contrib(contrib):
     print("t")
